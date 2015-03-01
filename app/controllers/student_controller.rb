e5cfa6a27d8ba5e485f6be3292433cf5c60687e6
Frank Licea <francisco.licea@gmail.com>
2015-03-01 02:59:02 -0600

    Ensure students only see their account

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,8 +1,15 @@
 class StudentController < ApplicationController
   before_action :ensure_admin_staff_or_student
+  before_action :set_student
 
   def show
     @student = current_user
     @opportunities = Opportunity.includes(:company).order(created_at: :desc).limit(25)
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
+    return if current_user.admin_or_staff?
+    redirect_home(current_user) unless @student == current_user
   end
 end
