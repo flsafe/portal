1dc0164d4bfa20b8b998084c0e9b09073124096d
Frank Licea <francisco.licea@gmail.com>
2015-03-19 14:20:50 -0600

    Remove ability to set oauth redirect uri via ENV

--- app/controllers/concerns/oauth.rb
+++ app/controllers/concerns/oauth.rb
@@ -9,8 +9,7 @@ module Oauth
     # oauth callback must be called with this returned
     session[:oauth_state] = SecureRandom.hex(16) 
     client_id = ENV['GITHUB_CLIENT_ID']
-    redirect_uri = URI.encode(ENV['GITHUB_REDIRECT_URI'] || github_url)
     # Application permission scope is only public data.
-    "https://github.com/login/oauth/authorize?client_id=#{client_id}&state=#{session[:oauth_state]}&redirect_uri=#{redirect_uri}"
+    "https://github.com/login/oauth/authorize?client_id=#{client_id}&state=#{session[:oauth_state]}"
   end
 end
